bdb88fce58898b5d103a3a8c4593ce18a0a805f1
Thirapa <po.thirapa_st@tni.ac.th>
2020-07-10 15:17:25 +0700

    first commit

---
+++ src/Extra.vue
@@ -0,0 +1,13 @@
+<template>
+  <h3>Extra Component</h3>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/TestComp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>HELLO WORLD2</h1>
+    <extra></extra>
+  </div>
+</template>
+
+<script>
+import Extra from './Extra.vue'
+export default {
+    components:{
+        Extra
+    }
+}
+</script>
+
+<style>
+
+</style>
